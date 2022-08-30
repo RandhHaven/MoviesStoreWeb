536077414e0a4705da65ef9e785f8dd50b9fc56e
Randhall Rangel <randhall.rd@gmail.com>
2022-08-30 15:46:56 -0300

    add login

--- movies-store-web/src/components/login/Login.jsx
+++ movies-store-web/src/components/login/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+//import './components/login/Login.css'
+//import email from './resources/images/email'
+//import favi from './favicon.svg'
 export const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <>
+      <div>
+        <h1>Login Page</h1>
+          <div>
+            <input type="text" placeholder='user name'/>
+          </div>
+          <div>
+            
+            <input type="password" placeholder='password'/>
+          </div>
+        </div>
+        <button>
+          Login
+        </button>
+        <div>
+          <p>
+            <a href="#"> Forgot password</a>
+          </p>
+          <p>
+            <a href="#"> Sign up </a>
+          </p>
+        </div>
+   
+    </>
   )
 }
--- movies-store-web/src/main.jsx
+++ movies-store-web/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {GifExpertApp} from './components/gifExpertApp/GifExpertApp'
+import { Login } from './components/login/Login'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GifExpertApp />
+    <Login />
   </React.StrictMode>
 )
