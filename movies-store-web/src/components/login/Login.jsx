b7b39fb79539938a49d3fcdf5d399c8af8339d0c
Randhall Rangel <randhall.rd@gmail.com>
2022-09-06 19:08:13 -0300

    add login

--- movies-store-web/src/components/login/Login.jsx
+++ movies-store-web/src/components/login/Login.jsx
@@ -1,33 +1,45 @@
 import React from 'react'
 import './Login.css'
-import email from '../resources/images/email.png'
+import email from '../resources/images/email.jpg'
+import password from '../resources/images/pass.png'
 import avatar from '../resources/images/avatar.png'
 export const Login = () => {
   return (
     <>
-      <div>
-        <h1>Login Page</h1>
-          <img src={avatar} alt='avatar' />
+      <div className='login-body'>
+        <div className='login-box'>
           <div>
-            <img src={email} alt='email' />
-            <input type="text" placeholder='user name'/>
-          </div>
-          <div>
-            
-            <input type="password" placeholder='password'/>
-          </div>
+            <div className="login-imgs">
+              <div className="container-image">
+                <img src={avatar} alt="profile" className="login-profile"/>
+              </div>
+            </div>
+            <div>
+              <h1>Login Page</h1>
+              <div>
+                <img className='login-email' src={email} alt='email' />
+                <input className="login-input-text" type="text" placeholder='user name'/>
+              </div>
+              <div className="login-pass-div">
+                <img className='login-pass' src={password} alt='email' />
+                <input className="login-input-text" type="password" placeholder='password'/>
+              </div>
+              <div className="login-button">
+                <button>Login</button>
+              </div>
+
+              <div>
+                <p className="login-link">
+                  <a href="#">Forgot password ?</a>
+                </p>
+                <p className="login-link">
+                  <a href="#">Sign Up</a>
+                </p>
+              </div> 
+            </div>
+          </div>            
         </div>
-        <button className='btn-login'>
-          Login
-        </button>
-        <div>
-          <p>
-            <a href="#"> Forgot password</a>
-          </p>
-          <p>
-            <a href="#"> Sign up </a>
-          </p>
-        </div>   
+      </div>
     </>
   )
 }
